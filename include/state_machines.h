73f9480095b567aec06b71f2e02eb39b6fb379d4
Lemon <77743782+lemon-gith@users.noreply.github.com>
2024-03-08 02:52:59 +0000

    move struct definition purely into header file

--- include/state_machines.h
+++ include/state_machines.h
@@ -1,7 +1,11 @@
 #ifndef STATE_MACHINES_H
 #define STATE_MACHINES_H
+// state machine types just need to be defined in headers 
+// to be included where needed
+
 
 #include "main.h"
+
 
 // a state machine to ensure good UX for button presses
 struct ButtonPress {
@@ -9,15 +13,28 @@ struct ButtonPress {
   enum State{OFF, ARMED, ON} state;
 
   // constructs a button state machine, given a name, starts OFF
-  ButtonPress(uint8_t knob_name);
+  ButtonPress(uint8_t knob_name){
+    this->knob_name = knob_name;
+    this->state = OFF;
+  }
 
   // based on whether or not button is currently pressed,
   // will determine and return the next state
   // (if just state progression is desired, can ignore result)
-  State nextState(bool isPressed);
+  State nextState(bool isPressed){
+    if(!isPressed)  // if not pressed, it's off...
+      return this->state = OFF;  // will it work without `this->`?
+    else 
+      if (this->state == OFF)  // if pressed and was OFF, must trigger
+        return this->state = ARMED;
+      else  // if pressed and was already pressed, it is ON
+        return this->state = ON;
+  }
 
   // just returns the current state, nothing else
-  State getState();
+  State getState(){
+    return state;
+  }
 };
 
 
