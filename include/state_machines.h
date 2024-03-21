1446e5108abbe4c7617b8699d76c947e5b18ca06
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2024-03-21 09:13:05 +0000

    add idea for joystick flick SM

--- include/state_machines.h
+++ include/state_machines.h
@@ -38,4 +38,76 @@ struct ButtonPress {
 };
 
 
+// a state machine for joystick flick toggling
+struct JoystickFlick {
+  // base, up-right, right-up, right-down, etc.
+  enum Direction{B, UR, RU, RD, DR, DL, LD, LU, UL} direction;
+  // OFF for B, else ARMED for change, ON for no change
+  enum State{OFF, ARMED, ON} state;
+  // enumerates 5 possible joystick direction configs
+  enum Config{UD, LR, UDLR, D8, D16};  // D16 not implemented yet
+
+  // constructs a button state machine, given a name, starts OFF
+  JoystickFlick(){
+    this->direction = B;
+    this->state = OFF;
+  }
+
+  // TODO: write this better, this seems table-like, think abt that
+  
+  // TODO: write a good description
+  State nextState(Direction dir, Config config){
+    if(dir == B){  // if not pressed, it's off...
+      direction = B;
+      return state = OFF;
+    }
+    else switch(config){
+      case UD:
+        if ((dir < RD || dir > LD)
+        && (this->direction < RD || this->direction > LD))
+          return this->state = ARMED;
+        else
+          return this->state = ON;
+      if (dir != this->direction){
+        this->direction = dir;
+        return this->state = ARMED;
+      }
+      else  // if pressed and was already pressed, it is ON
+        return this->state = ON;
+  }
+  }
+
+  // just returns the current state, nothing else
+  State getState(){
+    return state;
+  }
+
+  // just returns the current direction, nothing else
+  Direction getDirection(){
+    return direction;
+  }
+};
+
+/* idea for joystick fuzzy boundary SM:
+
+state-driven, kinda, basically using a form of hysteresis?
+
+0 (+1)
+1 (<<)
+10 (<<)
+100 (+1)
+101 (<<)
+1010 (...)
+
+then to get a sum of values :D
+
+x = {binary hysteresis thing}
+total = 0
+for (sth i = 1; i < 0b10000; i <<= 1){
+  total += x & i;
+}
+
+sth. like that, maybe?
+*/
+
 #endif
